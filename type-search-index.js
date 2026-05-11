579b0e4dcac1b65362065f05bcf6e21c8c16767b
Manuel <manu09f.o@gmail.com>
2026-05-11 10:00:37 +0200

    JavaDoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Cliente"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Pedido"},{"p":"<Unnamed>","l":"Producto"},{"p":"<Unnamed>","l":"Repartidor"}];updateSearchResults();
